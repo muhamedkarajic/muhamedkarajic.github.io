97e148e47fcae72db75a2da4748ac3ce4163d4f6
Muhamed Karajic <35216938+muhamedkarajic@users.noreply.github.com>
2019-03-23 20:03:02 +0100

    Update

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const h1 = document.getElementById("paralex_h1");
 const p = document.getElementById("paralex_p");
 const button = document.getElementById("paralex_button");
-// const half = document.getElementById("half");
 
 const about = document.getElementById("about");
 const h2 = document.getElementById("paralex_h2");
@@ -14,6 +13,14 @@ const design_left = document.getElementById("design-left");
 
 const design_right = document.getElementById("design-right");
 const blink = document.getElementById("blink");
+
+const skills = document.getElementById("skills");
+
+
+
+
+
+
 
 
 
@@ -38,7 +45,6 @@ function applyParallex(obj, negativOffset, offset, translateY, rotateZ, rotateY)
     }
 }
 
-var offset = window.pageYOffset;
 function parallex() {
     offset = window.pageYOffset;
 
@@ -51,14 +57,65 @@ function parallex() {
     applyParallex(h2, about.offsetTop, offset, .40, 0, 0);
     applyParallex(p_left, about.offsetTop, offset, .35, 0,0);
     applyParallex(p_right, about.offsetTop, offset, .30, 0, 0);
-    // applyParallex(half, about.offsetTop, offset, 0, 0, 0);
 
     applyParallex(h3, design.offsetTop, offset, .20, 0, 0);
     applyParallex(design_left, design.offsetTop, offset, .15, 0, 0);
     applyParallex(design_right, design.offsetTop, offset, .10, 0, 0);
 
 }
+const html = document.getElementById("html");
+const css = document.getElementById("css");
+const js = document.getElementById("js");
+const cpp = document.getElementById("cpp");
+const wordpress = document.getElementById("wordpress");
+const sololean = document.getElementById("sololean");
+const datacamp = document.getElementById("datacamp");
+const pluralsight = document.getElementById("pluralsight");
 
+function applyEffects(obj, ms)
+{
+    setTimeout(() => {
+        obj.classList.add("fadeIn");
+    }, ms);
+}
+
+
+function removeEffect(obj)
+{
+    obj.classList.add("fadeOut");
+}
+
+var offset = window.pageYOffset;
+function effects() {
+    offset = window.pageYOffset;
+    if(offset > about.offsetTop-window.innerHeight/3)
+    {
+        applyEffects(h2, 100);
+        applyEffects(p_left, 500);
+        applyEffects(p_right, 1000);
+    }
+
+    if(offset > skills.offsetTop-window.innerHeight/1.45)
+    {
+        applyEffects(html, 100);
+        applyEffects(css, 200);
+        applyEffects(js, 300);
+        applyEffects(cpp, 400);
+        applyEffects(wordpress, 500);
+        applyEffects(sololean, 600);
+        applyEffects(datacamp, 700);
+        applyEffects(pluralsight, 800);
+    }
+
+
+    if(offset > design.offsetTop-window.innerHeight/3)
+    {
+        applyEffects(h3, 100);
+        applyEffects(design_left, 500);
+        applyEffects(design_right, 1000);
+    }
+}
+ 
 
 
 button.onclick = function (e) {
@@ -73,42 +130,11 @@ button.onclick = function (e) {
 
 window.onload = function()
 {
-    document.getElementById("paralex_h1").style.opacity = "0";
-    document.getElementById("paralex_p").style.opacity = "0";
-    document.getElementById("paralex_button").style.opacity = "0";
-    document.body.style.overflow = 'hidden';
+    applyEffects(h1, 100);
+    applyEffects(p, 500);
+    applyEffects(button, 1000);
 
-
-
-    setTimeout(function()
-    {
-        document.getElementById("paralex_h1").classList.add('fadeIn');
-    }, 100);
-
-    setTimeout(function()
-    {
-        document.getElementById("paralex_p").classList.add('fadeIn');
-    }, 500);
-    
-    setTimeout(function()
-    {
-        document.getElementById("paralex_button").classList.add('fadeIn');
-    }, 1000);
-
-    setTimeout(function()
-    {
-        document.getElementById("paralex_h1").style.opacity = "1";
-        document.getElementById("paralex_p").style.opacity = "1";
-        document.getElementById("paralex_button").style.opacity = "1";
-    
-        document.getElementById("paralex_h1").classList.remove("fadeIn");
-        document.getElementById("paralex_p").classList.remove("fadeIn");
-        document.getElementById("paralex_button").classList.remove("fadeIn");
-        document.body.style.overflow = 'visible';
-        
-
-        window.onscroll = parallex;
-    }, 2100);
+    window.onscroll = effects;
 }
 
 
