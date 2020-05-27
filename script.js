93b855eed422b456bb59e04edbe1ea04771b24a0
Muhamed Karajic <muhamed.karajic@edu.fit.ba>
2020-05-27 06:31:55 +0100

    Update of animation render logic

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+var sections = document.getElementsByClassName("section");
+var last = sections[0];
 var timeOuts = new Array();
-var last = 0;
-var lastIndex = -1;
 var fp_auto_height = [];
 var fp_auto_height_string = 'fp-auto-height';
-var section = document.getElementsByClassName("section");
 
 function applyEffect(x, ms) {
     let delay = x.dataset.delay;
@@ -21,8 +20,7 @@ function removeEffect(obj) {
     obj.classList.remove(obj.dataset.animation);
 }
 
-function applyEffects(x, ms, element) {
-
+function applyEffects(x, ms) {
     for (let j = 0; j < timeOuts.length; j++)
         clearTimeout(timeOuts[j]);
     timeOuts = [];
@@ -42,36 +40,29 @@ var myFullpage = new fullpage('#fullpage', {
     navigation: true,
     licenseKey: 'bobwH@p8',
     onLeave: function (anchorLink, index) {
-        if (index != lastIndex) {
-            console.log(section[index-1]);
-            applyEffects(section[index-1].getElementsByClassName("animate"), 1000);
-        }
+        let newSection = sections[index - 1];
+        applyEffects(newSection.getElementsByClassName("animate"), 1000);
     },
 
     afterLoad: function (anchorLink, index) {
-        if (index != lastIndex) {
-            if (!this.classList.contains(fp_auto_height_string)) {
-                for (let j = 0; j < fp_auto_height.length; j++)
-                    removeEffects(fp_auto_height[j].getElementsByClassName("animate"));
-                fp_auto_height = [];
-                if (last != 0)
-                    removeEffects(last.getElementsByClassName("animate"));
-                last = this;
-                lastIndex = index;
+        let currentSection = sections[index - 1];
+
+        if (!currentSection.classList.contains('fp-auto-height')) {
+            for (let i = 0; i < sections.length; i++) {
+                const section = sections[i];
+                if (i != index - 1)
+                    removeEffects(section.getElementsByClassName("animate"));
             }
-            else
-                fp_auto_height.push(this);
-        }
-        else if (fp_auto_height.length > 0) {
             for (let j = 0; j < fp_auto_height.length; j++)
                 removeEffects(fp_auto_height[j].getElementsByClassName("animate"));
             fp_auto_height = [];
         }
+        last = currentSection;
     }
 });
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     setTimeout(function () {
-        applyEffects(document.getElementsByClassName("section")[0].getElementsByClassName("animate"), 1000);
-    }, parseInt(500))
+        applyEffects(last.getElementsByClassName("animate"), 1000);
+    }, 500)
 });
